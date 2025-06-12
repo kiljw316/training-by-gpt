f1626919ce31dd846de6b26ccdf98380739a02cf
JaewonKil <kiljw316@gmail.com>
2025-06-13 01:44:05 +0900

    first commitˆ

---
+++ src/test/java/com/example/trainingbygpt/TrainingByGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingbygpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingByGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
