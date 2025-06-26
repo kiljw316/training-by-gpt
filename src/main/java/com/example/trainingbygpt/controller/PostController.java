08e3d77f9b45c72c6621fbf850fa02550a65b919
JaewonKil <kiljw316@gmail.com>
2025-06-26 23:53:46 +0900

    chore: 메서드 위치 변경

--- src/main/java/com/example/trainingbygpt/controller/PostController.java
+++ src/main/java/com/example/trainingbygpt/controller/PostController.java
@@ -18,6 +18,11 @@ public class PostController {
 
     private final PostService postService;
 
+    @PostMapping
+    public ResponseDto savePost(@RequestBody @Valid PostSaveRequest request) {
+        return created(postService.savePost(request, 1L));
+    }
+
     @GetMapping
     public ResponseDto getPosts() {
         return ok(postService.getPosts());
